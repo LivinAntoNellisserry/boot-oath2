a4ddd88a8b73d83ea63544f584f7450b8a128a36
Livin <livin.nellisserry@gmail.com>
2021-12-22 16:57:32 +0530

    initial commit

---
+++ src/main/java/com/boot/oauth2/BootOath2Application.java
@@ -0,0 +1,13 @@
+package com.boot.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOath2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/oauth2/BootOath2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootOath2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
